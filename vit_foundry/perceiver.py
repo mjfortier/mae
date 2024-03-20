98eeee877a32065769267554cdaa6f0a69da55f0
matt <matt@localhost.localdomain>
2024-03-20 07:34:07 -0400

    added perceiver dataloader

--- vit_foundry/perceiver.py
+++ vit_foundry/perceiver.py
@@ -38,46 +38,82 @@ class FourierFeatureMapping(nn.Module):
         return torch.cat([sin_emb, cos_emb], dim=-1)
 
 
-class PerceiverDataset(Dataset):
-    def __init__(self, met_csv, flux_csv, context_window_length, target='NEE'):
-        self.target = target
-        self.met = pd.read_csv(met_csv, index_col=False).astype(np.float32)
-        self.flux = pd.read_csv(flux_csv, index_col=False).astype(np.float32)
-        self.flux = self.flux[self.flux[target].notna()]
-        self.context = context_window_length
+class FluxDataset(Dataset):
+    def __init__(self, data_dir, sites, context_length=48, targets=['GPP_NT_VUT_REF'], device='cpu'):
+        self.data_dir = data_dir
+        self.sites = sites
+        self.data = []
+        self.context_length = context_length
+        self.targets = targets
+        self.remove_columns = ['timestamp', 'NEE_VUT_REF', 'GPP_NT_VUT_REF', 'RECO_NT_VUT_REF']
+        self.device = device
+        
+        for root, _, files in os.walk(self.data_dir):
+            in_sites = False
+            for site in sites:
+                if site in root:
+                    in_sites = True
+            if not in_sites:
+                continue
+
+            if 'data.csv' in files:
+                df = pd.read_csv(os.path.join(root, 'data.csv'))
+                df['timestamp'] = pd.to_datetime(df['timestamp'])
+                with open(os.path.join(root, 'modis.pkl'), 'rb') as f:
+                    modis_data = pkl.load(f)
+                with open(os.path.join(root, 'meta.json'), 'r') as f:
+                    meta = json.load(f)
+
+                self.data.append((meta, df, modis_data))
+        
+        self.lookup_table = []
+        for i, d in enumerate(self.data):
+            _, df, _ = d
+            for r in range(self.context_length, len(df)+1):
+                self.lookup_table.append((i,r))
+
 
     def __len__(self):
-        return len(self.flux) - self.context
-    
-    def get_columns(self):
-        return list(self.met.drop('year', axis=1).columns)
+        return len(self.lookup_table)
 
     def __getitem__(self, idx):
-        flux_row = self.flux.iloc[idx]
-        year = flux_row['year']
-        doy = flux_row['doy']
-        minutes = flux_row['minutes']
-        row_condition = (self.met['year'] == year) & (self.met['doy'] == doy) & (self.met['minutes'] == minutes)
+        site_num, row_max = self.lookup_table[idx]
+        row_min = row_max - (self.context_length)
 
-        met_row_id = self.met[row_condition].index[0]
-
-        start, end = max(met_row_id - self.context + 1, 0), met_row_id + 1
-        met_rows = self.met.iloc[start:end]
-        flux_row = flux_row.drop('year')
-        met_rows = met_rows.drop('year', axis=1)
+        _, df, modis = self.data[site_num]
+        rows = df.iloc[row_min:row_max]
+        rows = rows.reset_index(drop=True)
+        modis_data = []
+        timestamps = list(rows['timestamp'])
+        for i, ts in enumerate(timestamps):
+            pixels = modis.get(ts, None)
+            if pixels is not None:
+                modis_data.append((i, torch.tensor(pixels[:,1:9,1:9]).to(self.device)))
         
-        # Padding for lack of observations
-        met = torch.tensor(met_rows.values)
-        flux = torch.tensor(flux_row[self.target])
-        
-        steps, obs = met.shape
-        row_deficit = self.context - steps
-        if row_deficit > 0:
-            padding = torch.empty((row_deficit, obs), dtype=met.dtype)
-            padding[:,:] = float('nan')
-            met = torch.cat((padding, met), 0)
+        targets = torch.tensor(rows[self.targets].values).to(self.device)
+        row_values = torch.tensor(rows.drop(columns=self.remove_columns).values)
+        mask = row_values.isnan().to(self.device)
+        row_values = row_values.nan_to_num(-1.0).to(self.device) # just needs a numeric value, doesn't matter what
 
-        return met, flux, met.isnan()
+        return row_values, mask, modis_data, targets
+
+
+def custom_collate_fn(batch):
+    row_values, mask, modis_data, targets = zip(*batch)
+
+    # imgs are tensors with the same dim, can be stacked
+    row_values = torch.stack(row_values, dim=0)
+    mask = torch.stack(mask, dim=0)
+    targets = torch.stack(targets, dim=0)
+
+    # masks and classes have variable size per sample, so they get returned as a list
+    modis_data = [m for m in modis_data]
+
+    return row_values, mask, modis_data, targets
+
+def FluxDataLoader(data_dir, sites, context_length = 48, targets=['GPP_NT_VUT_REF'], device='cpu', **kwargs):
+    ds = FluxDataset(data_dir, sites, context_length=context_length, targets=targets, device=device)
+    return DataLoader(ds, collate_fn=custom_collate_fn, **kwargs)
 
 
 class Perceiver(nn.Module):
@@ -153,46 +189,98 @@ class Perceiver(nn.Module):
         return loss.mean()
 
 
-context_length = 48
 
-dataset = PerceiverDataset('scotty_creek_met_processed.csv', 'scotty_creek_flux_processed.csv', context_length)
-dl = DataLoader(dataset, batch_size=16, shuffle=True)
 
-config = PerceiverConfig(
-    inputs=dataset.get_columns(),
-    context_length=context_length,
-    num_frequencies=12,
-    input_embedding_dim=8,
-    latent_hidden_dim=128,
-    num_heads=8,
-    layers = ('cross', 'self', 'cross', 'self', 'cross', 'self', 'self', 'self')
-)
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = Perceiver(config)
-model.to(device)
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.0001)
 
-def train_one_epoch(model, dataloader, optimizer):
-    total_loss = 0.0
-    for met, flux, mask in tqdm(dataloader):
-        try:
-            met = met.nan_to_num(-9999.0).to(device)
-            flux = flux.to(device)
-            mask = mask.to(device)
 
-            op = model(met, flux, mask)
 
-            loss = op['loss']
-            total_loss += loss.item()
+### Old code
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        except Exception as e:
-            print(flux)
-    return total_loss / len(dataloader)
+# class PerceiverDataset(Dataset):
+#     def __init__(self, met_csv, flux_csv, context_window_length, target='NEE'):
+#         self.target = target
+#         self.met = pd.read_csv(met_csv, index_col=False).astype(np.float32)
+#         self.flux = pd.read_csv(flux_csv, index_col=False).astype(np.float32)
+#         self.flux = self.flux[self.flux[target].notna()]
+#         self.context = context_window_length
 
-for x in range(5):
-    loss = train_one_epoch(model, dl, optimizer)
-    print(loss)
+#     def __len__(self):
+#         return len(self.flux) - self.context
+    
+#     def get_columns(self):
+#         return list(self.met.drop('year', axis=1).columns)
+
+#     def __getitem__(self, idx):
+#         flux_row = self.flux.iloc[idx]
+#         year = flux_row['year']
+#         doy = flux_row['doy']
+#         minutes = flux_row['minutes']
+#         row_condition = (self.met['year'] == year) & (self.met['doy'] == doy) & (self.met['minutes'] == minutes)
+
+#         met_row_id = self.met[row_condition].index[0]
+
+#         start, end = max(met_row_id - self.context + 1, 0), met_row_id + 1
+#         met_rows = self.met.iloc[start:end]
+#         flux_row = flux_row.drop('year')
+#         met_rows = met_rows.drop('year', axis=1)
+        
+#         # Padding for lack of observations
+#         met = torch.tensor(met_rows.values)
+#         flux = torch.tensor(flux_row[self.target])
+        
+#         steps, obs = met.shape
+#         row_deficit = self.context - steps
+#         if row_deficit > 0:
+#             padding = torch.empty((row_deficit, obs), dtype=met.dtype)
+#             padding[:,:] = float('nan')
+#             met = torch.cat((padding, met), 0)
+
+#         return met, flux, met.isnan()
+
+
+
+
+
+# context_length = 48
+
+# dataset = PerceiverDataset('scotty_creek_met_processed.csv', 'scotty_creek_flux_processed.csv', context_length)
+# dl = DataLoader(dataset, batch_size=16, shuffle=True)
+
+# config = PerceiverConfig(
+#     inputs=dataset.get_columns(),
+#     context_length=context_length,
+#     num_frequencies=12,
+#     input_embedding_dim=8,
+#     latent_hidden_dim=128,
+#     num_heads=8,
+#     layers = ('cross', 'self', 'cross', 'self', 'cross', 'self', 'self', 'self')
+# )
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# model = Perceiver(config)
+# model.to(device)
+# optimizer = torch.optim.AdamW(model.parameters(), lr=0.0001)
+
+# def train_one_epoch(model, dataloader, optimizer):
+#     total_loss = 0.0
+#     for met, flux, mask in tqdm(dataloader):
+#         try:
+#             met = met.nan_to_num(-9999.0).to(device)
+#             flux = flux.to(device)
+#             mask = mask.to(device)
+
+#             op = model(met, flux, mask)
+
+#             loss = op['loss']
+#             total_loss += loss.item()
+
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
+#         except Exception as e:
+#             print(flux)
+#     return total_loss / len(dataloader)
+
+# for x in range(5):
+#     loss = train_one_epoch(model, dl, optimizer)
+#     print(loss)
 
