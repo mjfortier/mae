0d65dceda29789cf833ed6b7aeb30cd686a6e9b6
matt <matt@localhost.localdomain>
2024-04-26 09:55:12 -0400

    time series validation dataloader

--- vit_foundry/perceiver/dataset.py
+++ vit_foundry/perceiver/dataset.py
@@ -130,6 +130,158 @@ def custom_collate_fn(batch):
     return predictor_values, predictor_labels[0], predictor_mask, time_values, time_labels[0], modis_list, target_values, target_labels[0]
 
 
+class FluxTimeSeriesValidationDataLoader():
+    def __init__(
+            self, data_dir, sites, context_length=48,
+            batch_size=32,
+            target_columns=['NEE_VUT_REF'],
+            time_columns=['DOY', 'TOD'],
+            remove_columns=['timestamp', 'NEE_VUT_REF', 'GPP_NT_VUT_REF', 'RECO_NT_VUT_REF'],
+            ):
+        self.data_dir = data_dir
+        self.sites = sites
+        self.context_length = context_length
+        self.batch_size = batch_size
+        self.data = []
+
+        self.target_columns = target_columns.copy()
+        self.time_columns = time_columns.copy()
+        self.remove_columns = remove_columns.copy()
+        [self.remove_columns.remove(c) for c in self.target_columns]
+
+        self.current_files = []
+
+        for root, _, files in os.walk(self.data_dir):
+            in_sites = False
+            for site in sites:
+                if site in root:
+                    in_sites = True
+            if not in_sites:
+                continue
+
+            if 'data.csv' in files:
+                df = pd.read_csv(os.path.join(root, 'data.csv'))
+
+                float_cols = [c for c in df.columns if c != 'timestamp']
+                df[float_cols] = df[float_cols].astype(np.float32)
+                df['timestamp'] = pd.to_datetime(df['timestamp'])
+                gt_df = df[['timestamp'] + target_columns].copy()
+                df[float_cols] = df[float_cols].astype(np.float32)
+                df[self.target_columns] = np.nan
+                df[self.target_columns] = df[self.target_columns].astype(np.float32)
+
+                with open(os.path.join(root, 'modis.pkl'), 'rb') as f:
+                    modis_data = pkl.load(f)
+                with open(os.path.join(root, 'meta.json'), 'r') as f:
+                    meta = json.load(f)
+
+                self.data.append({
+                    'meta': meta,
+                    'df': df,
+                    'gt_df': gt_df,
+                    'modis_data': modis_data,
+                    'final_row': len(df),
+                    'current_row': self.context_length - 1,
+                    'finished': False
+                })
+        self.set_dataset_len()
+    
+    def reset_files(self):
+        for file in self.data:
+            file['df'][self.target_columns] = np.nan
+            file['df'][self.target_columns] = file['df'][self.target_columns].astype(np.float32)
+            file['current_row'] = self.context_length
+            file['finished'] = False
+        self.set_current_files()
+
+    def set_dataset_len(self):
+        self.reset_files()
+        i = 0
+        while len(self.current_files) > 0:
+            dirty_current_file_list = False
+            for file in self.current_files:
+                file['current_row'] += 1
+                if file['current_row'] > len(file['df']):
+                    file['finished'] = True
+                    dirty_current_file_list = True
+            if dirty_current_file_list:
+                self.set_current_files()
+            i += 1
+        self.len = i
+        self.reset_files()
+
+    def __len__(self):
+        return self.len
+    
+    def set_current_files(self):
+        # Set the current_files array with up to {context_length} unfinished files
+        for file in self.data:
+            if file['finished']:
+                if file in self.current_files:
+                    self.current_files.remove(file)
+                continue
+            if file not in self.current_files and len(self.current_files) < self.batch_size:
+                self.current_files.append(file)
+    
+    def update_inferred_values(self, output_values):
+        dirty_current_file_list = False
+        if type(output_values) != list:
+            output_values = [output_values]
+        for file, target_values in zip(self.current_files, output_values):
+            file['df'].loc[file['current_row']-1, self.target_columns] = target_values
+            file['current_row'] += 1
+            if file['current_row'] > len(file['df']):
+                file['finished'] = True
+                dirty_current_file_list = True
+        if dirty_current_file_list:
+            self.set_current_files()
+    
+    def get_batch(self):
+        ### This is what needs to be finished
+        batch = []
+        for file in self.current_files:
+            row_max = file['current_row']
+            row_min = row_max - self.context_length
+            rows = file['df'].iloc[row_min:row_max]
+            rows = rows.reset_index(drop=True)
+
+            modis_data = []
+            timestamps = list(rows['timestamp'])
+            for i, ts in enumerate(timestamps):
+                pixels = file['modis_data'].get(ts, None)
+                if pixels is not None:
+                    modis_data.append((i, torch.tensor(pixels[:,1:9,1:9], dtype=torch.float32)))
+        
+            predictor_df = rows.drop(columns=self.remove_columns + self.time_columns)
+            time_df = rows[self.time_columns]
+            target_df = rows[self.target_columns]
+        
+            predictor_values = torch.tensor(predictor_df.values)
+            predictor_labels = list(predictor_df.columns) # may be different from self.tabular_columns
+            predictor_mask = predictor_values.isnan()
+            predictor_values = predictor_values.nan_to_num(-1.0) # just needs a numeric value, doesn't matter what
+
+            time_values = torch.tensor(time_df.values)
+            time_labels = list(time_df.columns)
+
+            target_values = torch.tensor(target_df.values[-1:])
+            target_labels = list(target_df.columns)
+            batch.append([
+                predictor_values, predictor_labels, predictor_mask, time_values, time_labels, modis_data, target_values, target_labels
+            ])
+        return custom_collate_fn(batch) if len(batch) > 0 else []
+
+    def batches(self):
+        self.reset_files()
+        batch = self.get_batch()
+        while len(batch) > 0:
+            yield batch
+            batch = self.get_batch()
+
+    def __iter__(self):
+        return self.batches()
+        
+
 def FluxDataLoader(data_dir, sites, context_length=48, target_columns=['NEE_VUT_REF'], time_series=False, **kwargs):
     ds = FluxDataset(data_dir, sites, context_length=context_length, target_columns=target_columns, time_series=time_series)
     return DataLoader(ds, collate_fn=custom_collate_fn, **kwargs)
